20dc0a32b5aeeac04ed03a2ce41bcbfe3160bff4
Mia Salem <miathewiccancatgirl@gmail.com>
2024-04-06 05:15:18 -0400

    ss

--- pkgs/ags-wrap/config/main.ts
+++ pkgs/ags-wrap/config/main.ts
@@ -239,93 +239,6 @@ const Right = Widget.Box({
     })]
 })
 
-/** @param {import('resource:///com/github/Aylur/ags/service/notifications.js').Notification} n */
-const NotificationIcon = ({ app_entry, app_icon, image }) => {
-  if (image) {
-    return Widget.Box({
-      css: `
-                background-image: url("${image}");
-                background-size: contain;
-                background-repeat: no-repeat;
-                background-position: center;
-            `,
-    });
-  }
-
-  let icon = 'dialog-information-symbolic';
-  if (Utils.lookUpIcon(app_icon))
-    icon = app_icon;
-
-  if (app_entry && Utils.lookUpIcon(app_entry))
-    icon = app_entry;
-
-  return Widget.Icon(icon);
-};
-
-const Notification = n => {
-
-  // @ts-ignore
-  const icon = Widget.Box({
-    vpack: 'start',
-    class_name: 'icon',
-    child: NotificationIcon(n),
-  });
-
-  const title = Widget.Label({
-    class_name: 'title',
-    xalign: 0,
-    justification: 'center',
-    hexpand: true,
-    max_width_chars: 24,
-    truncate: 'end',
-    wrap: true,
-    label: n.summary,
-    use_markup: true,
-  });
-
-  const body = Widget.Label({
-    class_name: 'body',
-    hexpand: true,
-    use_markup: true,
-    xalign: 0,
-    justification: 'left',
-    label: n.body,
-    wrap: true,
-  });
-
-  const actions = Widget.Box({
-    class_name: 'actions',
-    children: n.actions.map(({ id, label }) => Widget.Button({
-      class_name: 'action-button',
-      on_clicked: () => n.invoke(id),
-      hexpand: true,
-      child: Widget.Label(label),
-    })),
-  });
-
-  return Widget.EventBox({
-    child: Widget.Box({
-      class_name: `notification ${n.urgency}`,
-      vertical: true,
-      children: [
-        Widget.Box({
-          children: [
-            icon,
-            Widget.Box({
-              vertical: true,
-              children: [
-                title,
-                body,
-              ],
-            }),
-          ],
-        }),
-        actions,
-      ],
-    }),
-  });
-};
-
 const Bar = (monitor: number) => Widget.Window({
   monitor,
   name: `bar${monitor}`,
@@ -341,17 +254,17 @@ const Bar = (monitor: number) => Widget.Window({
   }),
 });
 
-const InfoBar = Widget.Window({
-  name: 'infobar',
-  anchor: ['top'],
-  layer: "overlay",
-  child: Widget.Box({
-    class_name: 'infobar',
-    css: 'padding: 1px;',
-    vertical: true,
-    children: notif.as(gay => gay.map(Notification)),
-  }),
-});
+// const InfoBar = Widget.Window({
+//   name: 'infobar',
+//   anchor: ['top'],
+//   layer: "overlay",
+//   child: Widget.Box({
+//     class_name: 'infobar',
+//     css: 'padding: 1px;',
+//     vertical: true,
+//     children: notif.as(gay => gay.map(Notification)),
+//   }),
+// });
 
 App.config({
   icons: "./assets",
@@ -359,7 +272,7 @@ App.config({
   windows: [
     Bar(0),
     // InfoBar,
-    // notificationPopup
+    NotificationPopups()
   ],
 });
 
