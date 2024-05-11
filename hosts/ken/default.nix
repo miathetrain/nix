aecced138dc47c7322b01f8da8ca01d6a3f4388c
Mia <miathewiccancatgirl@gmail.com>
2024-05-11 04:06:46 -0400

    hungry

--- hosts/ken/default.nix
+++ hosts/ken/default.nix
@@ -36,7 +36,7 @@
       # if the builder supports building for multiple architectures,
       # replace the previous line by, e.g.
       # systems = ["x86_64-linux" "aarch64-linux"];
-      maxJobs = 1;
+      maxJobs = 5;
       speedFactor = 2;
       supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
       mandatoryFeatures = [];
