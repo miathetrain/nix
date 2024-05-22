9cff340c3b163f1bf3e2c429440442f4681f14b0
Mia <miathewiccancatgirl@gmail.com>
2024-05-22 01:39:49 -0400

    update

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -8,9 +8,7 @@ with lib; {
   config = {
     #Graphics Drivers
     boot.initrd.kernelModules = mkIf (config.networking.hostName == "dreamhouse") ["amdgpu"];
-    services.xserver.videoDrivers = mkIf (config.networking.hostName == "dreamhouse") ["amdgpu"];
-    services.xserver.videoDrivers = mkIf (config.networking.hostName == "blossom") ["nvidia"];
-
+    services.xserver.videoDrivers = if (config.networking.hostName == "dreamhouse") then ["amdgpu"] else ["nvidia"];
     systemd.tmpfiles.rules = mkIf (config.networking.hostName == "dreamhouse") [
       "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
     ];
